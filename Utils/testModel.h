643e4ff41f2ddaf058c05c5be5a6f5022c7f8c37
🐰2 <a2@demacbook-pro.local>
2018-07-03 11:13:24 +0800

    test push

---
+++ Utils/testModel.h
@@ -0,0 +1,13 @@
+//
+//  testModel.h
+//  testApp
+//
+//  Created by 🐰 on 2018/7/3.
+//  Copyright © 2018年 🐰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testModel : NSObject
+
+@end
---
+++ Utils/testModel.m
@@ -0,0 +1,13 @@
+//
+//  testModel.m
+//  testApp
+//
+//  Created by 🐰 on 2018/7/3.
+//  Copyright © 2018年 🐰. All rights reserved.
+//
+
+#import "testModel.h"
+
+@implementation testModel
+
+@end
